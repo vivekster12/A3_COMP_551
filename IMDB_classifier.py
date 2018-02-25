e57bcdd6a3ccf8bd77e04782aeac4e1578bc1afc
Vivek Gidla <vivek.gidla@mail.mcgill.ca>
2018-02-24 23:22:16 -0500

    no changes, used to make plots

--- IMDB_classifier.py
+++ IMDB_classifier.py
@@ -7,6 +7,7 @@ import collections
 import time
 import sklearn.metrics as sk
 from sklearn.naive_bayes import BernoulliNB
+import matplotlib.pyplot as plt
 
 
 def create_bin_bag_of_words(corpus,dictionary):
@@ -46,15 +47,26 @@ def get_true_rating(corpus):
 	return (true_rating)
 
 def do_Naive_Bayes_Bernoulli(train_BoW, testing_BoW, train_true_rating, testing_true_rating):
-	smoothing = np.arange(1,10,1)
+	smoothing = [1.0]
+	f1_list = []
 	for i in smoothing:
 		print ("i:	",i)
 		clf = BernoulliNB(alpha = i)
-		clf.fit(IMDB_Bin_BoW_train,train_true_rating)
-		pred_arr = clf.predict(IMDB_Bin_BoW_valid)
+		clf.fit(train_BoW,train_true_rating)
+		pred_arr = clf.predict(testing_BoW)
 		f1_score = sk.f1_score(testing_true_rating, pred_arr, average='micro')
-		print (f1_score)
-	# return (f1_score)
+		f1_list.append(f1_score)
+		# print (f1_score)
+	
+	# plt.plot(smoothing,f1_list)
+	# plt.xlabel('Smoothing Coefficient - alpha')
+	# plt.axvline(x=smoothing[np.argmax(f1_list)],linestyle='dashed', color = 'black')
+	# plt.axhline(y=np.amax(f1_list),linestyle='dashed', color = 'black')
+	# plt.ylabel('F-Measure')
+	# plt.show()
+	print (smoothing[np.argmax(f1_list)])
+	print (np.amax(f1_list))
+
 	
 
 
@@ -68,9 +80,9 @@ if __name__ == "__main__":
 	IMDB_corpus_valid = pd.read_csv(r'/Users/vivek/git/A3_COMP_551/IMDB_Datasets/IMDB-valid.txt',encoding='utf-8',header = None,sep='\t')
 	IMDB_Bin_BoW_valid = create_bin_bag_of_words(IMDB_corpus_valid, IMDB_dictionary)
 	print ("created Bag of Words for validation")
+	IMDB_corpus_test = pd.read_csv(r'/Users/vivek/git/A3_COMP_551/IMDB_Datasets/IMDB-test.txt',encoding='utf-8',header = None,sep='\t')
+	IMDB_Bin_BoW_test = create_bin_bag_of_words(IMDB_corpus_test, IMDB_dictionary)
 
-
-
-	do_Naive_Bayes_Bernoulli(IMDB_Bin_BoW_train, IMDB_Bin_BoW_valid, get_true_rating(IMDB_corpus_train), get_true_rating(IMDB_corpus_valid))
+	do_Naive_Bayes_Bernoulli(IMDB_Bin_BoW_train, IMDB_Bin_BoW_test, get_true_rating(IMDB_corpus_train), get_true_rating(IMDB_corpus_test))
 	
 	print (time.clock() - start)
